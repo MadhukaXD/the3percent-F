affb7dfa74f5b685d5dc8ee132ab9d0cb1b3d877
MadhukaXD <madhuka@xdigital.solutions>
2022-02-21 21:00:15 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <h1>Food Microservice</h1>
       </header>
     </div>
